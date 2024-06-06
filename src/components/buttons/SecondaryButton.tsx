f6cfad961ca9da53860b43fd3ee10bb563b01f30
Kingsley Victor <javaprodigy56@gmail.com>
2024-06-06 20:28:33 +0100

    add secondary button

---
+++ src/components/buttons/SecondaryButton.tsx
@@ -0,0 +1,23 @@
+import React, { ButtonHTMLAttributes } from "react";
+import clsx from "clsx";
+
+interface SecondaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
+
+const SecondaryButton: React.FC<SecondaryButtonProps> = ({ disabled, children, ...props }) => {
+  return (
+    <button
+      {...props}
+      disabled={disabled}
+      className={clsx("w-full rounded-[100px] flex justify-center items-center p-[10px_20px] shrink-0 border", {
+        "border-[#edeeef] focus:border-[#fafbfc]": !disabled,
+        "border-[#d9d9d9]": disabled,
+      })}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SecondaryButton;
--- src/components/buttons/index.stories.tsx
+++ src/components/buttons/index.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta } from "@storybook/react";
 import UIKitPrimaryButton from "./PrimaryButton";
+import UIKitSecondaryButton from "./SecondaryButton";
 import React from "react";
 
 const meta: Meta<typeof UIKitPrimaryButton> = {
@@ -17,6 +18,20 @@ export const PrimaryButton: React.FC = () => {
       <UIKitPrimaryButton disabled>
         <span style={{ color: "#fff", textTransform: "capitalize" }}>disabled</span>
       </UIKitPrimaryButton>
+    </div>
+  );
+};
+
+export const SecondaryButton: React.FC = () => {
+  return (
+    <div style={{ width: "200px", display: "flex", flexDirection: "column", gap: "12px", backgroundColor: "#2054f4" }}>
+      <UIKitSecondaryButton>
+        <span style={{ color: "#fff", textTransform: "capitalize" }}>active</span>
+      </UIKitSecondaryButton>
+
+      <UIKitSecondaryButton disabled>
+        <span style={{ color: "#fff", textTransform: "capitalize" }}>disabled</span>
+      </UIKitSecondaryButton>
     </div>
   );
 };
