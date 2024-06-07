956b0232794cff44899e718ff8a5663d381362cd
Kingsley Victor <javaprodigy56@gmail.com>
2024-06-07 12:47:50 +0100

    add tabs

---
+++ src/components/navigation/Tabs.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement, cloneElement, useCallback, useEffect, useMemo } from "react";
+import clsx from "clsx";
+import { isFragment } from "react-is";
+
+interface TabProps {
+  onPress?: (index?: number) => void;
+  isActive?: boolean;
+  children?: React.ReactNode;
+  tabIndex?: number;
+}
+
+export const Tab: React.FC<TabProps> = ({ onPress, isActive, children, tabIndex }) => (
+  <button
+    onClick={() => onPress && onPress(tabIndex)}
+    className={clsx("inline-flex justify-center items-center p-[5px_10px] bg-[#003cf6] rounded-[100px]", {
+      "bg-[#003cf6]": isActive,
+      "bg-transparent": !isActive,
+    })}
+  >
+    {children}
+  </button>
+);
+
+interface TabsProps {
+  onSingleTabPress?: (index?: number) => void;
+  activeTabIndex?: number;
+  children: ReactElement<TabProps> | ReactElement<TabProps>[];
+}
+
+export const Tabs: React.FC<TabsProps> = ({ onSingleTabPress, activeTabIndex = 0, children }) => {
+  const tabs = useMemo(() => (Array.isArray(children) ? Array.from(children) : Array.from([children])), [children]);
+
+  const validateChildren = useCallback(() => {
+    const valid = tabs.every((tab) => tab.type === Tab || isFragment(tab));
+    return valid;
+  }, [tabs]);
+
+  useEffect(() => {
+    if (!validateChildren()) throw new Error("invalid child(ren) for Tabs");
+  }, [validateChildren]);
+
+  return (
+    <div className="flex justify-center items-center p-[5px_15px] rounded-[100px] gap-3">
+      {tabs.map((tab, index) =>
+        cloneElement(tab, {
+          key: index,
+          tabIndex: index,
+          isActive: activeTabIndex === index,
+          onPress: onSingleTabPress ? onSingleTabPress : tab.props.onPress,
+        })
+      )}
+    </div>
+  );
+};
---
+++ src/components/navigation/index.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta } from "@storybook/react";
+import { Tabs as UIKitTabs, Tab as UIKitTab } from "./Tabs";
+import React, { useState } from "react";
+
+const meta: Meta<typeof UIKitTabs> = {
+  component: UIKitTabs,
+  title: "components/navigation",
+};
+
+export default meta;
+
+export const Tabs: React.FC = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  return (
+    <div
+      style={{
+        width: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "12px",
+      }}
+    >
+      <UIKitTabs onSingleTabPress={(index) => setActiveTabIndex(index ?? 0)} activeTabIndex={activeTabIndex}>
+        {[...Array(4)].map((_, index) => (
+          <UIKitTab key={index}>
+            <span style={{ color: index === activeTabIndex ? "#fff" : "#434343", textTransform: "capitalize" }}>
+              tab {index + 1}
+            </span>
+          </UIKitTab>
+        ))}
+      </UIKitTabs>
+      {activeTabIndex === 0 && <span style={{ color: "#000" }}>Hello from tab on index 0</span>}
+      {activeTabIndex === 1 && <span style={{ color: "#000" }}>Another tab</span>}
+      {activeTabIndex === 2 && <span style={{ color: "#000" }}>A tab again</span>}
+      {activeTabIndex === 3 && <span style={{ color: "#000" }}>Final tab</span>}
+    </div>
+  );
+};
